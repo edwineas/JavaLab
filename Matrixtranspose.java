d7c64fa357a627b646969ed761f355fc87377dd7
EDWIN ALEX SHAJI <96735074+edwineas@users.noreply.github.com>
2022-11-30 14:56:57 +0530

    Update Matrixtranspose.java

--- Matrixtranspose.java
+++ Matrixtranspose.java
@@ -10,8 +10,8 @@ class Matrixtranspose
       int m = sc.nextInt();
       int mat[][] = new int[n][m];
       System.out.println("Enter the elements of the matrix");
-      for (i=0;i<n;i++)
-         for(j=0;j<m;j++)
+      for (i=0;i<m;i++)
+         for(j=0;j<n;j++)
             mat[i][j] = sc.nextInt();
       System.out.println("Transpose of the matrix");
       for (i=0;i<n;i++)
