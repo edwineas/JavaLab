fad001402a328269000b2672cac4fcb5c884e0d3
edwineas <edwinalexshaji@gmail.com>
2022-12-03 23:01:25 +0530

    2

---
+++ Addtwonumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Addtwonumbers
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the first number :\t");
+        int n1 =  sc.nextInt();
+        System.out.print("Enter the second number :\t");
+        int n2 =  sc.nextInt();
+        System.out.println("Sum = "+(n1+n2));
+    }
+}
