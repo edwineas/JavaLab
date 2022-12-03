7d905c8cf5c64be5a5cef94f690aa1ba9c76eb4f
edwineas <edwinalexshaji@gmail.com>
2022-12-04 01:26:14 +0530

    3

--- factors.java
+++ factors.java
@@ -3,11 +3,11 @@ class factors
 {
     public static void main(String args[])
     {
-        int sum,i;
+        int i;
         Scanner sc = new Scanner (System.in);
         System.out.print("Enter the number :\t");
         int n =  sc.nextInt();
-        for(i=1,sum=0;i<=n/2;i++)
+        for(i=1;i<=n/2;i++)
             if(n%i==0)
                 System.out.print(i+" ");
         System.out.println(n);
