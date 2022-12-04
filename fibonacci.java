d11899d18ea926a8c692324667cdc8195f68180d
edwineas <edwinalexshaji@gmail.com>
2022-12-04 12:00:27 +0530

    s

--- fibonacci.java
+++ fibonacci.java
@@ -7,12 +7,19 @@ class fibonacci
         Scanner sc = new Scanner (System.in);
         System.out.print("Enter the limit :\t");
         c =  sc.nextInt();
-        for(i=2;i<=c;++i)
+        System.out.print("0 1");
+        for(i=2;i<=c+1;++i)
         {
             n3=n1+n2;
-            System.out.print(" "+n3);
-            n1=n2;
-            n2=n3;
+            if(n3<=c)
+            {
+                System.out.print(" "+n3);
+                n1=n2;
+                n2=n3;
+            }
+            else
+                break;
+            
         };
     }
 }
