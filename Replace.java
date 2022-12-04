434ef463790dc286e61e21f0aeb67037c78b7154
edwineas <edwinalexshaji@gmail.com>
2022-12-04 14:41:27 +0530

    d

--- Replace.java
+++ Replace.java
@@ -4,16 +4,16 @@ class Replace
          public static void main(String args[])
          {
                   Scanner sc = new Scanner(System.in);
-                  System.out.println("Enter the size of the array");
+                  System.out.print("Enter the size of the array\t");
                   int n=sc.nextInt();
                   int arr[] = new int[n];
                   int i,flag=0;
                   System.out.println("Enter the elements");
                   for(i=0;i<n;i++)
                            arr[i]=sc.nextInt();
-                  System.out.println("Enter the element to be replaced");                  
+                  System.out.print("Enter the element to be replaced\t");                  
                   int e1=sc.nextInt();
-                  System.out.println("Enter the element to be replaced with");                  
+                  System.out.print("Enter the element to be replaced with\t");                  
                   int e2=sc.nextInt();
                   for(i=0;i<n;i++)
                            if (arr[i]==e1)
@@ -27,7 +27,7 @@ class Replace
                   {
                            System.out.println("Replaced array is");
                            for(i=0;i<n;i++)
-                                    System.out.println(arr[i]);
+                                    System.out.print(arr[i]+" ");
                   } 
 	}
 }
