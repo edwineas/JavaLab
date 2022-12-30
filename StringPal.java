b3249e4fe978e3c07a118f80b675545654d04b1c
edwineas <edwinalexshaji@gmail.com>
2022-12-30 19:03:39 +0530

    Create StringPal.java

---
+++ StringPal.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class StringPal
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string");
+        string s1=sc.next()
+    }
+}
