262f71915df6dbaea0c765b2568729d7c2ba66c6
edwineas <edwinalexshaji@gmail.com>
2023-01-04 06:58:36 +0530

    Update StringPal.java

--- StringPal.java
+++ StringPal.java
@@ -5,6 +5,20 @@ class StringPal
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string");
-        string s1=sc.next()
+        String s1=sc.nextLine();
+        String s2=s1.toLowerCase();
+        int flag=0;
+        for(int i=0;i<(s2.length()/2);i++)
+        {
+            if(s2.charAt(i)!=s2.charAt(s2.length()-i-1))
+            {
+                flag=1;
+                break;
+            }
+        }
+            if(flag==0)
+                System.out.println(s1 + " is palindrome");
+            else
+                System.out.println(s1 + " is not palindrome");
     }
 }
