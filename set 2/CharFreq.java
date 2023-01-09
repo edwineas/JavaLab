f72198d990912f8286a7143cdcfc6c0cd16fc8a6
EDWIN ALEX SHAJI <96735074+edwineas@users.noreply.github.com>
2023-01-10 00:39:44 +0530

    Update CharFreq.java

--- set 2/CharFreq.java
+++ set 2/CharFreq.java
@@ -4,15 +4,15 @@ class CharFreq
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string");
-        String s1 = sc.nextLine();
-        String s2=s1.toLowerCase();
-        System.out.print("Enter the character \t");
-        char c=sc.next().charAt(0);
-        int freq=0;
-        for(int i=0;i<s2.length();i++)
-        	if(s2.charAt(i)==c)
-        		freq++;
+        	System.out.println("Enter the string");
+		String s1 = sc.nextLine();
+		String s2=s1.toLowerCase();
+		System.out.print("Enter the character \t");
+		char c=sc.next().charAt(0);
+		int freq=0;
+		for(int i=0;i<s2.length();i++)
+			if(s2.charAt(i)==c)
+				freq++;
 		System.out.println("frequency of the given character : "+freq);
 	}
 }
